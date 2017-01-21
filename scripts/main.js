1e9c5e77a66c1fbce073b9892d05993efc0eeafa
smoockpp <smoockpp@gmail.com>
2017-01-21 22:11:15 +0000

    url fix

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,7 @@ $(function() {
       treehouseRequest('https://teamtreehouse.com/nikolaynikolov2.json');
       github('https://api.github.com/users/smoockpp');
       break;
-    case 'nncreatives/index.html':
+    case '/nncreatives/index.html':
       treehouseRequest('https://teamtreehouse.com/nikolaynikolov2.json');
       github('https://api.github.com/users/smoockpp');
       break;
